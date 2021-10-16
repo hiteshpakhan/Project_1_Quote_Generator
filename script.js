c89b0fbbe8ee83186540ab1232c474d7c4d91b61
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-10-16 15:29:35 +0530

    a

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let apiQuots = [];
+
+function newQuote() {
+    // to pick the randome number 
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
 async function getQuots(){
-    const apiUrl = 'https://type.fit/api/quotes'
+    const apiUrl = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
-        apiQuots = await response.json();
-        console.log(apiQuots[12]);
+        apiQuotes = await response.json();   //we already assign the apiQuotes as a globle variable
+        newQuote();  //it will bring the any single quote out of them all
     }catch(error){
         console.log(error)
     }
